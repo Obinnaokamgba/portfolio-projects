75c899674bf5cd74d14f11220a92937baf06222f
Obinna Okamgba <58083168+Obinnaokamgba@users.noreply.github.com>
2023-04-20 09:11:04 +0100

    Update Data Analysis using SQL.sql

--- Data Analysis using SQL.sql
+++ Data Analysis using SQL.sql
@@ -1,5 +1,6 @@
 /* 
   This project is aimed at analyzing and gaining insights from a movie production dataset. 
   PostgreSql was used in carrying out this activity. 
+  Dataset was gotten from https://www.kaggle.com/datasets/danielgrijalvas/movies?resource=download
 */
 
